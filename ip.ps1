0643882bfcceb6c40e7a2ca46f23cdf0c9795f46
PeregrinTook <alexeykolobov@outlook.com>
2025-04-28 13:45:56 +0400

    modified:   ip.ps1

--- ip.ps1
+++ ip.ps1
@@ -1,3 +1,27 @@
 ﻿$adapterName = "Ethernet"
-$ip = (Get-NetIPAddress -InterfaceAlias $adapterName -AddressFamily IPv4 | Where-Object { $_.IPAddress -notlike "169.*" }).IPAddress
-Write-Output $ip
+
+# Получаем IP-адрес и маску подсети
+$adapterInfo = Get-NetIPAddress -InterfaceAlias $adapterName -AddressFamily IPv4 | Where-Object { $_.IPAddress -notlike "169.*" }
+
+# Извлекаем IP и маску подсети
+$ip = $adapterInfo.IPAddress
+$subnetMask = $adapterInfo.PrefixLength
+
+Write-Output "IP-адрес: $ip"
+Write-Output "Маска подсети: $subnetMask"
+
+# arp -a
+$ip_for_updating = Get-NetNeighbor | Where-Object {$_.LinkLayerAddress -eq "8c-89-a5-0f-aa-19"} | Select-Object -ExpandProperty IPAddress
+
+if ($ip_for_updating) {
+    Write-Output "Запись для IP найдена:"
+    # Write-Output $ip_for_updating
+    Test-Connection -ComputerName $ip_for_updating -Count 1
+} else {
+    Write-Output "Запись для IP $ip_for_updating не найдена или пуста."
+}
+
+# ping $ip_for_updating
+# arp -a
+
+# 100.66.72.97
