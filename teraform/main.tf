6cd6ba358ec55004f1597ba9b73d4569c0bb1087
Alex <you@example.com>
2025-05-14 20:14:30 +0400

    modified:   .gitignore 	new file:   config/cloud_init.yml 	new file:   config/network_config.yml 	modified:   teraform/main.tf

--- teraform/main.tf
+++ teraform/main.tf
@@ -11,22 +11,13 @@ provider "libvirt" {
   uri = "qemu:///system"
 }
 
+# Создаем сеть для виртуальной машины
 resource "libvirt_network" "vm_net" {
   name      = "vm_open_k8s"
   mode      = "open"
   domain    = "internal_k8s"
   addresses = ["192.168.100.0/24"]
 }
-
-# Ресурс для хранения данных виртуальной машины
-# resource "libvirt_pool" "accessible_pool" {
-#   name = "default"
-#   type = "dir"
-
-#   target {
-#     path = "/var/lib/libvirt/images/"
-#   }
-# }
 
 # Диск виртуальной машины
 resource "libvirt_volume" "ubuntu_disk" {
@@ -38,29 +29,25 @@ resource "libvirt_volume" "ubuntu_disk" {
 
 # Ресурс Cloud-init диск
 resource "libvirt_cloudinit_disk" "ubuntu_init" {
-  name      = "ubuntu-init.iso"
+  name      = "ubuntu-init-${timestamp()}.iso"
   pool      = "default"  # Используем уже существующий default pool
-  user_data = <<EOF
-#cloud-config
-users:
-  - name: ubuntu
-    sudo: ALL=(ALL) NOPASSWD:ALL
-    groups: sudo
-    ssh_authorized_keys:
-      - ${file("/home/alexkol/.ssh/id_rsa.pub")}
-EOF
+  user_data = data.template_file.user_data.rendered
+  network_config = data.template_file.network_config.rendered
+
   lifecycle {
     create_before_destroy = true
   }
 }
-
-
+# ggh45
 # Виртуальная машина
 resource "libvirt_domain" "ubuntu_vm" {
-  name   = "ubuntu_vm"
+  name   = "ubuntu_vm2"
   memory = 2048
   vcpu   = 2
-
+  network_interface {
+    network_name = "default"
+    hostname     = "ubuntu-vm-nat"
+  }
   network_interface {
     network_id = libvirt_network.vm_net.id
     hostname   = "ubuntu-vm"
@@ -84,4 +71,24 @@ resource "libvirt_domain" "ubuntu_vm" {
 
   # Подключение Cloud-init диска
   cloudinit = libvirt_cloudinit_disk.ubuntu_init.id
+}
+
+# Используем data source для шаблонов cloud-init и network-config
+
+data "template_file" "user_data" {
+  template = file("/home/alexkol/k8s_ubuntu2404server/config/cloud_init.yml")
+  vars = {
+    ssh_key = file("/home/alexkol/.ssh/id_rsa.pub")
+  }
+}
+
+
+data "template_file" "network_config" {
+  template = file("/home/alexkol/k8s_ubuntu2404server/config/network_config.yml")
+}
+
+variable "ssh_key" {
+  description = "Path to the public SSH key"
+  type        = string
+  default     = "/home/alexkol/.ssh/id_rsa.pub"  # Укажите путь к вашему публичному ключу
 }
