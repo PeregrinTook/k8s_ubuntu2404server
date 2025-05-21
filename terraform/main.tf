bab0e4f8c4a7ab90cdb9a4af757f86ab27306578
Alex <you@example.com>
2025-05-21 21:00:04 +0400

    modified:   terraform/main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,28 +18,25 @@ resource "libvirt_network" "vm_net" {
   autostart = true
 }
 
-# resource "libvirt_volume" "ubuntu_disk" {
-#   for_each = { for vm in var.vms : vm.hostname => vm }
+resource "libvirt_volume" "ubuntu_disk" {
+  for_each = { for vm in var.vms : vm.hostname => vm }
 
-#   name = "${each.value.hostname}_disk.qcow2"
-#   source = "${path.module}/../images/ubuntu-template.qcow2"
-#   pool   = libvirt_pool.k8s_vms_pool.name
-#   format = "qcow2"
-#   depends_on = [libvirt_pool.k8s_vms_pool]
-# }
+  name       = "${each.value.hostname}_disk.qcow2"
+  source     = "${path.module}/../images/ubuntu-template.qcow2"
+  pool       = libvirt_pool.k8s_vms_pool.name
+  format     = "qcow2"
+  depends_on = [libvirt_pool.k8s_vms_pool]
+}
 
-# output "debug_vms_map" {
-#   value = { for vm in var.vms : vm.hostname => vm }
-# }
-
-######################################
 resource "null_resource" "create_vm_dirs" {
   for_each = { for vm in var.vms : vm.hostname => vm }
 
   provisioner "local-exec" {
     command = "mkdir -p ${path.module}/../config_vms_autogen/${each.key}"
   }
+  depends_on = [libvirt_volume.ubuntu_disk]
 }
+
 resource "null_resource" "cleanup_vm_dirs" {
   for_each = { for vm in var.vms : vm.hostname => vm }
 
@@ -47,6 +44,7 @@ resource "null_resource" "cleanup_vm_dirs" {
     when    = destroy
     command = "rm -rf ${path.module}/../config_vms_autogen/${each.key}"
   }
+  depends_on = [libvirt_volume.ubuntu_disk]
 }
 
 resource "local_file" "cloud_init" {
@@ -67,59 +65,53 @@ resource "local_file" "network_config" {
   filename = "${path.module}/../config_vms_autogen/${each.key}/network_config.yml"
   content = templatefile("${path.module}/../templates/terraform/cloud_user_network_init/network_config.yml.tmpl", {
     ip_address_inner = each.value.ip_inner
-    # gateway    = var.gateway
   })
+  depends_on = [null_resource.create_vm_dirs]
 }
-# ###################################################
 
-# resource "libvirt_cloudinit_disk" "ubuntu_init" {
-#   for_each = { for vm in var.vms : vm.hostname => vm }
+resource "libvirt_cloudinit_disk" "ubuntu_init" {
+  for_each = { for vm in var.vms : vm.hostname => vm }
 
-#   name = "ubuntu-init-${each.value.hostname}.iso"
-#   pool = libvirt_pool.k8s_vms_pool.name
-#   user_data = templatefile("${path.module}/${each.value.config_path}/cloud_init.yml", {
-#     ssh_key  = file(var.ssh_key_path),
-#     hostname = each.value.hostname
-#   })
-#   network_config = file("${path.module}/${each.value.config_path}/network_config.yml")
+  name           = "ubuntu-init-${each.value.hostname}.iso"
+  pool           = libvirt_pool.k8s_vms_pool.name
+  user_data      = local_file.cloud_init[each.key].content
+  network_config = local_file.network_config[each.key].content
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-#   depends_on = [libvirt_pool.k8s_vms_pool]
-# }
+  lifecycle {
+    create_before_destroy = true
+  }
+  depends_on = [libvirt_pool.k8s_vms_pool, local_file.cloud_init, local_file.network_config]
+}
 
-# resource "libvirt_domain" "ubuntu_vm" {
-#   for_each = { for vm in var.vms : vm.name => vm }
+resource "libvirt_domain" "ubuntu_vm" {
+  for_each = { for vm in var.vms : vm.hostname => vm }
 
-#   name   = each.value.name
-#   memory = each.value.memory
-#   vcpu   = each.value.vcpu
+  name   = each.value.hostname
+  memory = each.value.memory
+  vcpu   = each.value.vcpu
 
-#   network_interface {
-#     network_name = "default"
-#     hostname     = each.value.hostname
-#   }
-#   console {
-#     type        = "pty"
-#     target_port = "0"
-#     target_type = "serial"
-#   }
+  network_interface {
+    network_name = "default"
+    hostname     = each.value.hostname
+  }
+  console {
+    type        = "pty"
+    target_port = "0"
+    target_type = "serial"
+  }
 
-#   network_interface {
-#     network_id = libvirt_network.vm_net.id
-#     hostname   = "${each.value.hostname}-private"
-#   }
+  network_interface {
+    network_id = libvirt_network.vm_net.id
+    hostname   = "${each.value.hostname}-private"
+  }
 
-#   disk {
-#     volume_id = libvirt_volume.ubuntu_disk[each.key].id
-#   }
+  disk {
+    volume_id = libvirt_volume.ubuntu_disk[each.key].id
+  }
 
-#   cloudinit  = libvirt_cloudinit_disk.ubuntu_init[each.key].id
-#   depends_on = [libvirt_pool.k8s_vms_pool]
-# }
-
-
+  cloudinit  = libvirt_cloudinit_disk.ubuntu_init[each.key].id
+  depends_on = [libvirt_pool.k8s_vms_pool]
+}
 
 # resource "local_file" "ansible_inventory" {
 
@@ -131,20 +123,17 @@ resource "local_file" "network_config" {
 #   depends_on = [libvirt_domain.ubuntu_vm]
 # }
 
-
-
-
-# output "VM_info" {
-#   value = {
-#     for vm in var.vms : vm.name => {
-#       name      = libvirt_domain.ubuntu_vm[vm.name].name
-#       memory    = libvirt_domain.ubuntu_vm[vm.name].memory
-#       vcpu      = libvirt_domain.ubuntu_vm[vm.name].vcpu
-#       mac_inner = libvirt_domain.ubuntu_vm[vm.name].network_interface[1].mac
-#       status    = libvirt_domain.ubuntu_vm[vm.name].running
-#       ip_inner  = vm.ip_inner
-#     }
-#   }
-# }
+output "VM_info" {
+  value = {
+    for vm in var.vms : vm.hostname => {
+      name      = libvirt_domain.ubuntu_vm[vm.hostname].name
+      memory    = libvirt_domain.ubuntu_vm[vm.hostname].memory
+      vcpu      = libvirt_domain.ubuntu_vm[vm.hostname].vcpu
+      mac_inner = libvirt_domain.ubuntu_vm[vm.hostname].network_interface[1].mac
+      status    = libvirt_domain.ubuntu_vm[vm.hostname].running
+      ip_inner  = vm.ip_inner
+    }
+  }
+}
 
 
