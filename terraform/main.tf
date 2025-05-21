ad1fba65d7599e3b832dbf63f6ecd0478c3e4ee5
Alex <you@example.com>
2025-05-21 20:37:52 +0400

    refractored tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,17 +6,9 @@ resource "libvirt_pool" "k8s_vms_pool" {
   type = "dir"
 
   target {
-    path = "/home/alexkol/k8s_ubuntu2404server/for_k8s_vms/"
+    path = abspath("${path.module}/../for_k8s_vms/")
   }
 }
-
-# resource "null_resource" "start_pool" {
-#   provisioner "local-exec" {
-#     command = "virsh pool-start for_k8s_vms"
-#   }
-#   depends_on = [libvirt_pool.k8s_vms_pool]
-# }
-
 
 resource "libvirt_network" "vm_net" {
   name      = "vm_open_k8s"
@@ -26,88 +18,133 @@ resource "libvirt_network" "vm_net" {
   autostart = true
 }
 
-resource "libvirt_volume" "ubuntu_disk" {
-  for_each = { for vm in var.vms : vm.name => vm }
+# resource "libvirt_volume" "ubuntu_disk" {
+#   for_each = { for vm in var.vms : vm.hostname => vm }
 
-  name = "${each.value.name}_disk.qcow2"
-  # pool = "default"
-  source = "/home/alexkol/k8s_ubuntu2404server/images/ubuntu-template.qcow2"
-  pool   = libvirt_pool.k8s_vms_pool.name
-  format = "qcow2"
-  # size   = 10 * 1024 * 1024 * 1024
-  depends_on = [libvirt_pool.k8s_vms_pool]
+#   name = "${each.value.hostname}_disk.qcow2"
+#   source = "${path.module}/../images/ubuntu-template.qcow2"
+#   pool   = libvirt_pool.k8s_vms_pool.name
+#   format = "qcow2"
+#   depends_on = [libvirt_pool.k8s_vms_pool]
+# }
+
+# output "debug_vms_map" {
+#   value = { for vm in var.vms : vm.hostname => vm }
+# }
+
+######################################
+resource "null_resource" "create_vm_dirs" {
+  for_each = { for vm in var.vms : vm.hostname => vm }
+
+  provisioner "local-exec" {
+    command = "mkdir -p ${path.module}/../config_vms/${each.key}"
+  }
+}
+resource "null_resource" "cleanup_vm_dirs" {
+  for_each = { for vm in var.vms : vm.hostname => vm }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "rm -rf ${path.module}/../config_vms/${each.key}"
+  }
 }
 
-resource "libvirt_cloudinit_disk" "ubuntu_init" {
-  for_each = { for vm in var.vms : vm.name => vm }
+resource "local_file" "cloud_init" {
+  for_each = { for vm in var.vms : vm.hostname => vm }
 
-  name = "ubuntu-init-${each.key}-${timestamp()}.iso"
-  # pool = "default"
-  pool = libvirt_pool.k8s_vms_pool.name
-  user_data = templatefile("${path.module}/${each.value.config_path}/cloud_init.yml", {
-    ssh_key  = file(var.ssh_key_path),
+  filename = "${path.module}/../config_vms/${each.key}/cloud_init.yml"
+  content  = templatefile("${path.module}/../templates/terraform/cloud_user_network_init/cloud_init.yml.tmpl", {
+    username = "k8s"
     hostname = each.value.hostname
+    ssh_key  = file(var.ssh_key_path)
   })
-  network_config = file("${path.module}/${each.value.config_path}/network_config.yml")
-
-  lifecycle {
-    create_before_destroy = true
-  }
-  depends_on = [libvirt_pool.k8s_vms_pool]
+  depends_on = [ null_resource.create_vm_dirs ]
 }
 
-resource "libvirt_domain" "ubuntu_vm" {
-  for_each = { for vm in var.vms : vm.name => vm }
+resource "local_file" "network_config" {
+  for_each = { for vm in var.vms : vm.hostname => vm }
 
-  name   = each.value.name
-  memory = each.value.memory
-  vcpu   = each.value.vcpu
-
-  network_interface {
-    network_name = "default"
-    hostname     = each.value.hostname
-  }
-  console {
-    type        = "pty"
-    target_port = "0"
-    target_type = "serial"
-  }
-
-  network_interface {
-    network_id = libvirt_network.vm_net.id
-    hostname   = "${each.value.hostname}-private"
-  }
-
-  disk {
-    volume_id = libvirt_volume.ubuntu_disk[each.key].id
-  }
-
-  cloudinit  = libvirt_cloudinit_disk.ubuntu_init[each.key].id
-  depends_on = [libvirt_pool.k8s_vms_pool]
-}
-resource "local_file" "ansible_inventory" {
-  filename = "${path.module}/../ansible/inventory.yml"
-  content = templatefile("${path.module}/../ansible/inventory_template.yml.tpl", {
-    master_ip  = var.vms[0].ip_inner,
-    worker_ips = slice([for vm in var.vms : vm.ip_inner], 1, length(var.vms))
+  filename = "${path.module}/../config_vms/${each.key}/network_config.yml"
+  content  = templatefile("${path.module}/../templates/terraform/cloud_user_network_init/network_config.yml.tmpl", {
+    ip_address_inner = each.value.ip_inner
+    # gateway    = var.gateway
   })
-  depends_on = [libvirt_domain.ubuntu_vm]
 }
+# ###################################################
+
+# resource "libvirt_cloudinit_disk" "ubuntu_init" {
+#   for_each = { for vm in var.vms : vm.hostname => vm }
+
+#   name = "ubuntu-init-${each.value.hostname}.iso"
+#   pool = libvirt_pool.k8s_vms_pool.name
+#   user_data = templatefile("${path.module}/${each.value.config_path}/cloud_init.yml", {
+#     ssh_key  = file(var.ssh_key_path),
+#     hostname = each.value.hostname
+#   })
+#   network_config = file("${path.module}/${each.value.config_path}/network_config.yml")
+
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+#   depends_on = [libvirt_pool.k8s_vms_pool]
+# }
+
+# resource "libvirt_domain" "ubuntu_vm" {
+#   for_each = { for vm in var.vms : vm.name => vm }
+
+#   name   = each.value.name
+#   memory = each.value.memory
+#   vcpu   = each.value.vcpu
+
+#   network_interface {
+#     network_name = "default"
+#     hostname     = each.value.hostname
+#   }
+#   console {
+#     type        = "pty"
+#     target_port = "0"
+#     target_type = "serial"
+#   }
+
+#   network_interface {
+#     network_id = libvirt_network.vm_net.id
+#     hostname   = "${each.value.hostname}-private"
+#   }
+
+#   disk {
+#     volume_id = libvirt_volume.ubuntu_disk[each.key].id
+#   }
+
+#   cloudinit  = libvirt_cloudinit_disk.ubuntu_init[each.key].id
+#   depends_on = [libvirt_pool.k8s_vms_pool]
+# }
+
+
+
+# resource "local_file" "ansible_inventory" {
+
+#   filename = "${path.module}/../ansible/inventory.yml"
+#   content = templatefile("${path.module}/../ansible/inventory_template.yml.tpl", {
+#     masters = [for vm in var.vms : vm if vm.k8s_role == "control-plane"],
+#     workers = [for vm in var.vms : vm if vm.k8s_role == "worker"]
+#   })
+#   depends_on = [libvirt_domain.ubuntu_vm]
+# }
 
 
 
 
-output "VM_info" {
-  value = {
-    for vm in var.vms : vm.name => {
-      name      = libvirt_domain.ubuntu_vm[vm.name].name
-      memory    = libvirt_domain.ubuntu_vm[vm.name].memory
-      vcpu      = libvirt_domain.ubuntu_vm[vm.name].vcpu
-      mac_inner = libvirt_domain.ubuntu_vm[vm.name].network_interface[1].mac
-      status    = libvirt_domain.ubuntu_vm[vm.name].running
-      ip_inner  = vm.ip_inner
-    }
-  }
-}
+# output "VM_info" {
+#   value = {
+#     for vm in var.vms : vm.name => {
+#       name      = libvirt_domain.ubuntu_vm[vm.name].name
+#       memory    = libvirt_domain.ubuntu_vm[vm.name].memory
+#       vcpu      = libvirt_domain.ubuntu_vm[vm.name].vcpu
+#       mac_inner = libvirt_domain.ubuntu_vm[vm.name].network_interface[1].mac
+#       status    = libvirt_domain.ubuntu_vm[vm.name].running
+#       ip_inner  = vm.ip_inner
+#     }
+#   }
+# }
 
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,28 +7,40 @@ variable "vms" {
   description = "List of VMs with unique configs"
   default = [
     {
-      name        = "k8s-m"
       hostname    = "k8s-m"
+      username    = "ks8"
       config_path = "../config/vm_k8s_m"
       memory      = 4096
       vcpu        = 2
       ip_inner    = "192.168.100.50"
+      k8s_role    = "control-plane"
     },
     {
-      name        = "k8s-w1"
       hostname    = "k8s-w1"
+      username    = "ks8"
       config_path = "../config/vm_k8s_w1"
       memory      = 4096
       vcpu        = 2
       ip_inner    = "192.168.100.51"
+      k8s_role    = "worker"
     },
     {
-      name        = "k8s-w2"
       hostname    = "k8s-w2"
+      username    = "ks8"
       config_path = "../config/vm_k8s_w2"
       memory      = 4096
       vcpu        = 2
       ip_inner    = "192.168.100.52"
+      k8s_role    = "worker"
+    },
+    {
+      hostname    = "k8s-m2"
+      username    = "ks8"
+      config_path = "../config/vm_k8s_m2"
+      memory      = 4096
+      vcpu        = 2
+      ip_inner    = "192.168.100.53"
+      k8s_role    = "control-plane"
     }
   ]
 }
